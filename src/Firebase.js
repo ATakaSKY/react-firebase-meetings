75632af543e053ffb04e1feb4cf4b0152b0bc6ee
Thakur <aakthakur@deloitte.com>
2018-12-16 13:38:54 +0530

    added firebase rules

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,12 +3,12 @@ import 'firebase/database';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCk9mIMo7jjWzJ2UYtA8435he3vl1mWdpc',
-  authDomain: 'react-spas.firebaseapp.com',
-  databaseURL: 'https://react-spas.firebaseio.com',
-  projectId: 'react-spas',
-  storageBucket: 'react-spas.appspot.com',
-  messagingSenderId: '579151738498'
+  apiKey: "AIzaSyBWS1mH61Ev1XzxMG5vjG23BfWTS9-GoAs",
+  authDomain: "react-spa-8b299.firebaseapp.com",
+  databaseURL: "https://react-spa-8b299.firebaseio.com",
+  projectId: "react-spa-8b299",
+  storageBucket: "react-spa-8b299.appspot.com",
+  messagingSenderId: "1067325630464"
 };
 
 firebase.initializeApp(config);
